9203e496d4934df5129b0dd30e98c9ff78c073d2
ciper <anmolkarki101@gmail.com>
2023-11-12 10:10:53 +0545

    Define routes using gorilla/mux for BookStore API

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/KarkiAnmol/BookStore-API-Golang/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{id}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{id}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{id}", controllers.DeleteBook).Methods("DELETE")
+
+}
