e0ba564360fe169a116da3d4eb29186b797d143b
ciper <anmolkarki101@gmail.com>
2023-11-13 11:06:59 +0545

    Add comments to controllers package for improved code documentation

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -11,39 +11,42 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var NewBook models.Book
-
+// GetBooks retrieves a list of books and sends it as a JSON response.
 func GetBooks(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetBooks()
 	res, _ := json.Marshal(newBooks)
-	w.Header().Set("Content-Type", "pkglication/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+// GetBookById retrieves details of a specific book by ID and sends it as a JSON response.
 func GetBookById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	bookId := vars["Id"]
 	ID, err := strconv.ParseInt(bookId, 0, 0)
 	if err != nil {
 		fmt.Println("error while parsing")
-
 	}
 	bookDetails, _ := models.GetBookById(ID)
 	res, _ := json.Marshal(bookDetails)
-	w.Header().Set("Content-Type", "pkglication/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
 
+// CreateBook creates a new book based on the request body and sends the created book as a JSON response.
 func CreateBook(w http.ResponseWriter, r *http.Request) {
 	CreateBook := &models.Book{}
 	utils.ParseBody(r, CreateBook)
 	b := CreateBook.CreateBook()
 	res, _ := json.Marshal(b)
-	w.Header().Set("Content-Type", "pkglication/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+// DeleteBook removes a book based on its ID and sends the deletion status as a JSON response.
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	Id := vars["ID"]
@@ -53,22 +56,27 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	}
 	deleteBook := models.DeleteBook(ID)
 	res, _ := json.Marshal(deleteBook)
-	w.Header().Set("Contet-Type", "pkglication/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
-func UpdateBook(w http.ResponseWriter, r *http.Request) {
 
+// UpdateBook modifies details of an existing book based on the request body and sends the updated book as a JSON response.
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	Id := vars["ID"]
 	ID, err := strconv.ParseInt(Id, 0, 0)
 	if err != nil {
 		fmt.Println("error while parsing")
 	}
+
 	updateBook := &models.Book{}
 	utils.ParseBody(r, updateBook)
 
+	// Retrieve the book details after deletion
 	bookDetails, db := models.GetBookById(ID)
+
+	// Update book details based on the request body
 	if updateBook.Name != "" {
 		bookDetails.Name = updateBook.Name
 	}
@@ -78,10 +86,13 @@ func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	if updateBook.Publication != "" {
 		bookDetails.Publication = updateBook.Publication
 	}
+
+	// Save the updated book details to the database
 	db.Save(&bookDetails)
+
+	// Send the updated book details as a JSON response
 	res, _ := json.Marshal(bookDetails)
-	w.Header().Set("Content-Type", "pkglication/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
-
 }
